6b896fbee8ef3fedff92c62bb27fa2e494428b73
Simone D'Angelo <sim.dangelo.95@gmail.com>
2023-12-17 12:27:43 +0100

    caching

--- src/main/scala/part3caching/Caching.scala
+++ src/main/scala/part3caching/Caching.scala
@@ -7,6 +7,7 @@ object Caching {
 
   val spark = SparkSession.builder()
     .appName("Caching")
+    .config("spark.sql.adaptive.enabled", "false")
     .config("spark.memory.offHeap.enabled", "true")
     .config("spark.memory.offHeap.size", 10000000)
     .master("local")
@@ -21,29 +22,30 @@ object Caching {
   // simulate an "expensive" operation
   val orderedFlightsDF = flightsDF.orderBy("dist")
 
+  flightsDF.count()
+
   // scenario: use this DF multiple times
-
   orderedFlightsDF.persist(
-    // no argument = MEMORY_AND_DISK
-    // StorageLevel.MEMORY_ONLY // cache the DF in memory EXACTLY - CPU efficient, memory expensive
-    // StorageLevel.DISK_ONLY // cache the DF to DISK - CPU efficient and mem efficient, but slower
-    // StorageLevel.MEMORY_AND_DISK // cache this DF to both the heap AND the disk - first caches to memory, but if the DF is EVICTED, will be written to disk
-
-    /* modifiers: */
-    // StorageLevel.MEMORY_ONLY_SER // memory only, serialized - more CPU intensive, memory saving - more impactful for RDDs
-    // StorageLevel.MEMORY_ONLY_2 // memory only, replicated twice - for resiliency, 2x memory usage
-    // StorageLevel.MEMORY_ONLY_SER_2 // memory only, serialized, replicated 2x
-
-    /* off-heap */
+//    // no argument = MEMORY_AND_DISK
+//    // StorageLevel.MEMORY_ONLY // cache the DF in memory EXACTLY - CPU efficient, memory expensive
+//    // StorageLevel.DISK_ONLY // cache the DF to DISK - CPU efficient and mem efficient, but slower
+//    // StorageLevel.MEMORY_AND_DISK // cache this DF to both the heap AND the disk - first caches to memory, but if the DF is EVICTED, will be written to disk
+//
+//    /* modifiers: */
+//    // StorageLevel.MEMORY_ONLY_SER // memory only, serialized - more CPU intensive, memory saving - more impactful for RDDs
+//    // StorageLevel.MEMORY_ONLY_2 // memory only, replicated twice - for resiliency, 2x memory usage
+//     StorageLevel.MEMORY_ONLY_SER_2 // memory only, serialized, replicated 2x
+//
+//    /* off-heap */
     StorageLevel.OFF_HEAP // cache outside the JVM, done with Tungsten, still stored on the machine RAM, needs to be configured, CPU efficient and memory efficient
   )
-
+//
   orderedFlightsDF.count()
   orderedFlightsDF.count()
-
-  // remove from cache
+//
+//  // remove from cache
   orderedFlightsDF.unpersist() // remove this DF from cache
-
+//
   // change cache name
   orderedFlightsDF.createOrReplaceTempView("orderedFlights")
   spark.catalog.cacheTable("orderedFlights")
